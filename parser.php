f165e38cde8395fc0a5bdd2628548cb9f3dbaa66
mihajlo <mihajlo88@gmail.com>
2017-03-26 01:29:03 +0100

    ispravno prosledjivanje, cini mise; sad parsiranje svakog dela pojedinacno

--- parser.php
+++ parser.php
@@ -35,24 +35,26 @@ function main($celine='')
 
 	$handle = fopen("knjiga-latinica.txt", "r");
 	if ($handle) {
+
+		$flag = "";
+
 	    while (($line = fgets($handle)) !== false) {
 	        //provera da li se neka od kljucnih reci nalazi
 
-			foreach ($celine as $key => $value) {
-		
-				$subject = $line;
-				$pattern = '/^\s*'.$value.'/';
-				$res = preg_match($pattern, $subject);
+	    	$status = provera( $celine, $line);
 
-				if($res === 1){
-					$key( trim( $line )  );
-					break;
-					//die();
-				}
-				//if()
-				//regex provera ^\s+ sadrzaj value-a
-				//ako postoji stream ide u funkciju; break foreach
-			}
+	    	
+
+	    	if( !empty($status) ){ //novi flag
+	    		$flag = $status;
+	    	}
+	    	else if($flag !== ""){ //obicna linija
+	    		$flag( $line );
+	    	}
+
+			
+			//if($flag !== "")
+				
 	    }
 
 	    fclose($handle);
@@ -64,6 +66,30 @@ function main($celine='')
 }
 
 
+//treda da vrati aktivan flag i status
+function provera($niz='', $line)
+{
+	foreach ($niz as $key => $value ) {
+		
+		$subject = $line;
+		$pattern = '/^\s*'.$value.'/';
+		$res = preg_match($pattern, $subject);
+
+		if($res === 1){
+			$key( trim( $line )  );		// upisi prvu liniju	
+			return $key; //flag, koji je aktivan
+			//break; //izlazi iz foreach-a
+			//die();
+		}
+
+
+		//regex provera ^\s+ sadrzaj value-a
+		//ako postoji stream ide u funkciju; break foreach
+	}
+
+	return false; //nije nijedna od pocetnih linija, obicna je linija
+}
+
 //kada naidjes na 1 stavku to je novi element
 
 
@@ -72,82 +98,82 @@ function main($celine='')
 
 function StudijskiProgram($value='')
 {
-	print_r("StudijskiProgram ". $value);
+	print_r("StudijskiProgram ". $value . "\n");
 	# funkcija br 1
 }
 function VrstaStudija($value='')
 {
-	print_r("VrstaStudija ". $value);
+	print_r("VrstaStudija ". $value . "\n");
 	# funkcija br 2
 }
 function NazivPredmeta($value='')
 {
-	print_r("NazivPredmeta ". $value);
+	print_r("NazivPredmeta ". $value . "\n");
 	# funkcija br 3
 }
 function Nastavnik($value='')
 {
-	print_r("Nastavnik ". $value);
+	print_r("Nastavnik ". $value . "\n");
 	# funkcija br 4
 }
 function Status($value='')
 {
-	print_r("Status ". $value);
+	print_r("Status ". $value . "\n");
 	# funkcija br 5
 }
 function ESPB($value='')
 {
-	print_r("ESPB ". $value);
+	print_r("ESPB ". $value . "\n");
 	# funkcija br 6
 }
 function Uslov($value='')
 {
-	print_r("Uslov ". $value);
+	print_r("Uslov ". $value . "\n");
 	# funkcija br 7
 }
 function CiljPredmeta($value='')
 {
-	print_r("CiljPredmeta ". $value);
+	print_r("CiljPredmeta ". $value . "\n");
 	# funkcija br 8
 }
 function Ishod($value='')
 {
-	print_r("Ishod ". $value);
+	print_r("Ishod ". $value . "\n");
 	# funkcija br 9
 }
 function SadrzajPredmeta($value='')
 {
-	print_r("SadrzajPredmeta ". $value);
+	print_r("SadrzajPredmeta ". $value . "\n");
 	# funkcija br 10
 }
 function TeorijskaNastava($value='')
 {
-	print_r("TeorijskaNastava ". $value);
+	print_r("TeorijskaNastava ". $value . "\n");
 	# funkcija br 11
 }
 function PrakticnaNastava($value='')
 {
-	print_r("PrakticnaNastava ". $value);
+	print_r("PrakticnaNastava ". $value . "\n");
 	# funkcija br 12
 }
 function Literatura($value='')
 {
-	print_r("Literatura ". $value);
+	print_r("Literatura ". $value . "\n");
 	# funkcija br 13
 }
 function BrojCasova($value='')
 {
-	print_r("BrojCasova ". $value);
+	print_r("BrojCasova ". $value . "\n");
 	# funkcija br 14
 }
 function MetodaIzvodjena($value='')
 {
-	print_r("MetodaIzvodjena ". $value);
+	print_r("MetodaIzvodjena ". $value . "\n");
 	# funkcija br 15
 }
 function Ocena($value='')
 {
-	print_r("Ocena ". $value);
+	print_r("Ocena ". $value . "\n");
 	# funkcija br 16
 }
 
