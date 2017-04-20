f345cd83bff1585e5dbf3e618603540ca7f0aef1
mihajlo <mihajlo88@gmail.com>
2017-04-20 22:11:11 +0200

    buffer globalni ne radi lepo

--- parser.php
+++ parser.php
@@ -21,9 +21,15 @@
 */
 
 
-global $brojac;
+
+mixaju se stvari 
+izbaciti globalni buffer,
+
+global $brojac, $buffer_flag, $buffer;
 
 $brojac = 0;
+$buffer = "";
+$buffer_flag = '';
 
 $celine = array( "StudijskiProgram" => "Studijski program" , "VrstaStudija" => "Vrsta i" , "NazivPredmeta" => "Naziv predmeta" , "Nastavnik" => "Nastavnik" , "Status" => "Status predmeta" , "ESPB" => "Broj ESPB" , "Uslov" => "Uslov:" , "CiljPredmeta" => "Cilj predmeta" , "Ishod" => "Ishod predmeta" , "SadrzajPredmeta" => "Sadržaj predmeta" , "TeorijskaNastava" => "Teorijska nastava" , "PrakticnaNastava" => "Praktična nastava" , "Literatura" => "Literatura" , "BrojCasova" => "Broj časova" , "MetodaIzvodjena" => "Metode izvođenja" , "Ocena" => "Ocena znanja"  );
 
@@ -34,6 +40,7 @@ function main($celine='')
 {
 	// liniju po liniju citas 
 	
+	global $buffer_flag;
 
 	$handle = fopen("knjiga-latinica.txt", "r");
 	if ($handle) {
@@ -49,15 +56,21 @@ function main($celine='')
 
 	    	if( !empty($status) ){ //novi flag
 	    		$flag = $status;
+	    		
+	    		//upisi multiline buffer
+	    		if($buffer_flag !='')
+	    			upisiBaffer( $celine );
 	    	}
 	    	else if($flag !== ""){ //obicna linija
-	    		$flag( $line );
+	    		$flag( $line ); //obradjuj sa prethodnim flagom
+
 	    	}
 
 			
 			//if($flag !== "")
 				
 	    }
+	    print_r("}");
 
 	    fclose($handle);
 	} else {
@@ -96,6 +109,18 @@ function provera($niz='', $line)
 //kada naidjes na 1 stavku to je novi element
 
 
+function upisiBaffer($celine='')
+{
+	global $brojac,$buffer_flag, $buffer;
+
+	$text = preg_replace("/[\r\n]+/", " ", $buffer);
+	$text = preg_replace("/\"/", "'", $text);
+	//uraditi replace newline-a sa razmakom
+	print_r( " id:". $brojac . " ,\n ". $buffer_flag.":\"". $text . "\",\n");
+	$buffer = '';
+	$buffer_flag = '';
+}
+
 //sve funkcije - parsiraju i upisuju text u posebne fajlove - gomila posebnih fajlova
 // na kraju se ti fajlovi merg-uju u jedan
 
@@ -105,7 +130,7 @@ function StudijskiProgram($value='')
 	$brojac++;
 
 	$upis = explode( "Studijski program/studijski programi:", $value);
-	print_r("{ id:". $brojac . " ,\n program:'". $upis[1] . "',\n");
+	print_r("},{ id:". $brojac . " ,\n program:\"". $upis[1] . "\",\n");
 	# funkcija br 1
 }
 function VrstaStudija($value='')
@@ -114,7 +139,7 @@ function VrstaStudija($value='')
 	global $brojac;
 
 	$upis = explode( "Vrsta i nivo studija:", $value);
-	print_r(" id:". $brojac . " ,\n vrstaStudija:'". $upis[1] . "',\n");
+	print_r(" id:". $brojac . " ,\n vrstaStudija:\"". $upis[1] . "\",\n");
 	# funkcija br 2
 }
 function NazivPredmeta($value='')
@@ -130,7 +155,7 @@ function NazivPredmeta($value='')
 	if(empty( $temp )) return;
 
 	if(empty($upis[1] ))
-		print_r(" id:". $brojac . " ,\n predmet:'". $temp . "',\n");
+		print_r(" id:". $brojac . " ,\n predmet:\"". $temp . "\",\n");
 	//print_r( $brojac ." NazivPredmeta ". $value . "\n");
 	# funkcija br 3
 }
@@ -141,7 +166,7 @@ function Nastavnik($value='')
 	$upis = explode( "Nastavnik:", $value);
 	
 	if( !empty( $upis[1] )  )
-		print_r(" id:". $brojac . " ,\n nastavnik:'". $upis[1] . "',\n");
+		print_r(" id:". $brojac . " ,\n nastavnik:\"". $upis[1] . "\",\n");
 	
 	# funkcija br 4
 }
@@ -151,7 +176,7 @@ function Status($value='')
 	//Status predmeta:Izborni
 	$upis = explode( "Status predmeta:", $value);
 	
-	print_r(" id:". $brojac . " ,\n status:'". $upis[1] . "',\n");
+	print_r(" id:". $brojac . " ,\n status:\"". $upis[1] . "\",\n");
 	# funkcija br 5
 }
 function ESPB($value='')
@@ -159,7 +184,7 @@ function ESPB($value='')
 	global $brojac;
 	$upis = explode( "Broj ESPB:", $value);
 	
-	print_r(" id:". $brojac . " ,\n ESPB:'". $upis[1] . "',\n");
+	print_r(" id:". $brojac . " ,\n ESPB:\"". $upis[1] . "\",\n");
 	# funkcija br 6
 }
 function Uslov($value='')
@@ -167,61 +192,130 @@ function Uslov($value='')
 	global $brojac;
 	$upis = explode( "Uslov:", $value);
 	
-	print_r(" id:". $brojac . " ,\n uslov:'". $upis[1] . "',\n");
+	print_r(" id:". $brojac . " ,\n uslov:\"". $upis[1] . "\",\n");
 	# funkcija br 7
 }
+
+
+
+
+
+//kompleksnije parsiranje
 function CiljPredmeta($value='')
 {
-	global $brojac;
-	print_r( $brojac ." CiljPredmeta ". $value . "\n");
-	# funkcija br 8
+	global $buffer, $buffer_flag;
+
+	$upis = explode( "Cilj predmeta", $value); //ako nema
+
+	$buffer .= $upis[0] . " ";		
+	//buffer
+	$buffer_flag = "CiljPredmeta";
+	
 }
+
+
+
+
 function Ishod($value='')
 {
-	global $brojac;
-	print_r( $brojac ." Ishod ". $value . "\n");
+
+	global $buffer, $buffer_flag;
+
+	$upis = explode( "Ishod predmeta", $value); //ako nema
+
+	$buffer .= $upis[0] . " ";		
+	//buffer
+	$buffer_flag = "Ishod";
+	/*global $brojac;
+	print_r( $brojac ." Ishod ". $value . "\n");*/
 	# funkcija br 9
 }
+
+
 function SadrzajPredmeta($value='')
 {
-	global $brojac;
-	print_r( $brojac ." SadrzajPredmeta ". $value . "\n");
+
+	
+
+	global $buffer, $buffer_flag;
+
+	$upis = explode( "Sadržaj predmeta", $value); //ako nema
+
+	$buffer .= $upis[0] . " ";		
+	//buffer
+	$buffer_flag = "SadrzajPredmeta";
 	# funkcija br 10
 }
 function TeorijskaNastava($value='')
 {
-	global $brojac;
-	print_r( $brojac ." TeorijskaNastava ". $value . "\n");
+		
+
+	global $buffer, $buffer_flag;
+
+	$upis = explode( "Teorijska nastava", $value); //ako nema
+
+	$buffer .= $upis[0] . " ";		
+	//buffer
+	$buffer_flag = "TeorijskaNastava";
 	# funkcija br 11
 }
 function PrakticnaNastava($value='')
 {
-	global $brojac;
-	print_r( $brojac ." PrakticnaNastava ". $value . "\n");
+
+	global $buffer, $buffer_flag;
+
+	$upis = explode( "Prakticna nastava:", $value); //ako nema
+
+	$buffer .= $upis[0] . " ";		
+	//buffer
+	$buffer_flag = "PrakticnaNastava";
+
 	# funkcija br 12
 }
 function Literatura($value='')
 {
-	global $brojac;
-	print_r( $brojac ." Literatura ". $value . "\n");
+	
+	global $buffer, $buffer_flag;
+
+	$upis = explode( "Literatura", $value); //ako nema
+
+	$buffer .= $upis[0] . " ";		
+	//buffer
+	$buffer_flag = "Literatura";
 	# funkcija br 13
 }
 function BrojCasova($value='')
 {
-	global $brojac;
-	print_r( $brojac ." BrojCasova ". $value . "\n");
+	global $buffer, $buffer_flag;
+
+	$upis = explode( "Broj časova aktivne nastave", $value); //ako nema
+
+	$buffer .= $upis[0] . " ";		
+	//buffer
+	$buffer_flag = "BrojCasova";
+
 	# funkcija br 14
 }
 function MetodaIzvodjena($value='')
 {
-	global $brojac;
-	print_r( $brojac ." MetodaIzvodjena ". $value . "\n");
+	global $buffer, $buffer_flag;
+	$upis = explode( "Metode izvođenja nastave", $value); //ako nema
+	$buffer .= $upis[0] . " ";		
+
+	$buffer_flag = "MetodaIzvodjena";
 	# funkcija br 15
 }
+
 function Ocena($value='')
 {
-	global $brojac;
-	print_r( $brojac ." Ocena ". $value . "\n");
+
+	
+	global $buffer, $buffer_flag;
+
+	$upis = explode( "Ocena znanja (maksimalni broj poena 100)", $value); //ako nema
+	$buffer .= $upis[0] . " ";		
+
+	$buffer_flag = "Ocena";
 	# funkcija br 16
 }
 
